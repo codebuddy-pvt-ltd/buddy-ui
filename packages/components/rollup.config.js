0fd1981b3b9fa96c33fc5b4b20211b12982ba050
Sohan Dutta <sohan@codebuddy.co>
2023-03-06 02:38:18 +0530

    :sparkles: feat(types): fix ui component package types

--- packages/components/rollup.config.js
+++ packages/components/rollup.config.js
@@ -1,34 +1,42 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
+import typescript2 from "rollup-plugin-typescript2";
 import { terser } from "rollup-plugin-terser";
-import external from "rollup-plugin-peer-deps-external";
+import peerExternal from "rollup-plugin-peer-deps-external";
 import nodeExternals from "rollup-plugin-node-externals";
+import dts from "rollup-plugin-dts";
 
 const packageJson = require("./package.json");
 
-export default {
-  input: "./src/index.ts",
-  output: [
-    {
-      file: packageJson.main,
-      format: "cjs",
-      sourcemap: true,
-      name: "@buddy-ui/components",
-    },
-    {
-      file: packageJson.module,
-      format: "esm",
-      sourcemap: true,
-    },
-  ],
-  external: ["@mantine/core", "@mantine/hooks"],
-  plugins: [
-    external(),
-    nodeExternals(),
-    resolve(),
-    commonjs(),
-    typescript({ tsconfig: "./tsconfig.json" }),
-    terser(),
-  ],
-};
+export default [
+  {
+    input: "./src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+        name: "@buddy-ui/components",
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    external: ["@mantine/core", "@mantine/hooks"],
+    plugins: [
+      peerExternal(),
+      nodeExternals(),
+      resolve(),
+      commonjs(),
+      typescript2({ tsconfig: "./tsconfig.json" }),
+      terser(),
+    ],
+  },
+  {
+    input: "./dist/esm/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts.default()],
+  },
+];
